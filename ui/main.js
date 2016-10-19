dc750eee41f5562e51e28fe36e1bd9cf0e444d03
jaxstronomer <jags.k.verma@gmail.com>
2016-10-19 16:46:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var button =document.getElementById('counter');
+var counter=0;
+button.onCLick = function(){
+  counter=counter+1;
+  var span = document.getElementById("count");
+  span.InnerHTML=counter.toString();
+  
+};
