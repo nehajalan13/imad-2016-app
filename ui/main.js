8bb1ed8c27e136f1b45b69c9b91d02ef4f2affb0
jaxstronomer <jags.k.verma@gmail.com>
2016-11-05 04:47:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,15 @@ function about(){
   req1.open('GET','/about',true);
   req1.send();
 }
+
+function articles(){
+  var element=document.getElementById('row3');
+  var req2=new XMLHttpRequest();
+  req1.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200){
+      element.innerHTML=this.responseText;
+    }
+  };
+  req2.open('GET','/articles',true);
+  req2.send();
+}
