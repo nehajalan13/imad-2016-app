2cdfbc6cf6b53d9994fcb8b54deda0ae4a454534
jaxstronomer <jags.k.verma@gmail.com>
2016-11-05 03:06:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/about',function(req,res){
           about
         </p>`
   res.send(about);
-}
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
