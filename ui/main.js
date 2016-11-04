130ba7753b1578c64b91c9893fce01868e5e9649
jaxstronomer <jags.k.verma@gmail.com>
2016-11-05 02:58:35 +0530

    Create main.js

---
+++ ui/main.js
@@ -0,0 +1,11 @@
+function about(){
+  var element=document.getElementsById('row3');
+  var req1=new XMLHttpRequest();
+  req1.onreadysrtatechange=function(){
+    if(this.readystate==4 && this.status==200){
+      element.innerHTML=this.responseText;
+    }
+  }
+  req1.open('GET','/about',true);
+  req1.send();
+}
