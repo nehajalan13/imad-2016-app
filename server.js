286b99a349d11172b2df2e3636a2a91ea4679c6d
jaxstronomer <jags.k.verma@gmail.com>
2016-11-05 04:38:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/common.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'common.css'));
 });
